cc78b67abd2d6730ae820fffe0465221e1c9091c
mahdi toosi <pugnacious0571@gmail.com>
2023-07-01 09:51:09 +0330

    refactor: users index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="ar" class="light-theme"  dir="rtl">
+<html lang="fa" dir="rtl">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,6 +19,7 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="app" class="wrapper">
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,61 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-
     <x-page.header>
-        <h4 class="mb-0">لیست کاربران</h4>
-        <a href="{{ route('users.create') }}" class="btn btn-primary">ایجاد کاربر جدید</a>
+        <form action="{{ route('users.index') }}" method="GET" class="w-100">
+            <div class="input-group">
+                <input type="text" class="form-control" name="keyword" placeholder="جستجو..." value="{{ $keyword }}">
+                <button class="btn btn-outline-secondary" type="submit">جستجو</button>
+            </div>
+        </form>
     </x-page.header>
 
-    <div class="raw mt-4">
-        <div class="col-md-12">
-            <div class="card bg-white">
-                <div class="card-body">
-                    <form action="{{ route('users.index') }}" method="GET" class="mt-3">
-                        <div class="input-group">
-                            <input type="text" class="form-control" name="keyword" placeholder="جستجو..." value="{{ $keyword }}">
-                            <button class="btn btn-outline-secondary" type="submit">جستجو</button>
-                        </div>
-                    </form>
+    <div class="card bg-white">
+        <div class="card-body">
 
-                    <table class="table table-striped mt-3">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>تصویر</th>
-                            <th>نام و نام خانوادگی</th>
-                            <th>ایمیل</th>
-                            <th>فعالیت ها</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($users as $user)
-                            <tr style="vertical-align: baseline;">
-                                <td>{{ $loop->iteration }}</td>
-                                <td>
-                                    <div class="avatar avatar-small">
-                                        <img src="{{ $user->avatar ? asset('storage/'.$user->avatar) : asset('/img/user.png')}}" alt="Avatar">
-                                    </div>
-                                </td>
-                                <td>{{ $user->first_name }} {{ $user->last_name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>
-                                    <a href="{{ route('users.invoices', $user->id) }}" class="btn btn-sm btn-info">صورتحساب ها</a>
-                                    <a href="{{ route('users.edit', $user) }}" class="btn btn-sm btn-primary">ویرایش</a>
-                                    <form action="{{ route('users.destroy', $user) }}" method="POST" style="display: inline-block;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger">حذف</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
 
-                    {{ $users->links() }}
+        @foreach($users as $user)
+            <a href="{{ route('users.invoices', $user->id) }}" class="d-flex text-body mb-2" style="gap: .5rem">
+                <div class="avatar avatar-small">
+                    <img src="{{ $user->avatar ? asset('storage/'.$user->avatar) : asset('/img/user.png')}}" alt="Avatar">
                 </div>
-            </div>
+
+                <div>
+                    <h6 class="mb-0">{{ $user->first_name }} {{ $user->last_name }}</h5>
+                    <small>{{$user->email}}</small>
+                </div>
+            </a>
+
+            <!-- <td>{{ $user->email }}</td>
+            <td>
+                <a href="{{ route('users.invoices', $user->id) }}" class="btn btn-sm btn-info">صورتحساب ها</a>
+                <a href="{{ route('users.edit', $user) }}" class="btn btn-sm btn-primary">ویرایش</a>
+                <form action="{{ route('users.destroy', $user) }}" method="POST" style="display: inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-sm btn-danger">حذف</button>
+                </form>
+            </td> -->
+        @endforeach
+
+            {{ $users->links() }}
         </div>
     </div>
+
+    <a href="{{ route('users.create') }}" class="btn btn-primary rounded-circle __add_user_btn">
+        <span class="fas fa-plus" style=""></span>
+    </a>
 @endsection
